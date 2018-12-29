9c00b13b5f622bb24b1d308d4e8232e15dbfdb57
Jamie Nunez <jdunn70@gtech.edu>
2018-12-29 10:21:49 -0800

    Refactoring

--- analysis.py
+++ analysis.py
@@ -12,7 +12,8 @@ import os
 import module as mod
 
 
-def main(path, fmat, delim, save=True, handdrawn=False, use_edges=False):
+def main(path, fmat, delim, save=True, handdrawn=False):
+    
     # Create output folder if needed
     save_path = os.path.join(path, 'output/')
     if not os.path.exists(save_path):
@@ -25,149 +26,80 @@ def main(path, fmat, delim, save=True, handdrawn=False, use_edges=False):
     
     for fname in files:
         res = process_image(path, fname, save_path, fmat, save=save,
-                            handdrawn=handdrawn, use_edges=use_edges)
+                            handdrawn=handdrawn)
         f.write('%s%s' % (res, '\n'))
     f.close()
 
-def _get_points(edges, x):
-    top_y = []
-    bot_y = []
-    for col in x:
-        points = np.where(edges[:, col] > 0)
-        if len(points[0]) > 0:
-            top_y.append(points[0][0])
-            bot_y.append(points[0][-1])
-        else:
-            top_y.append(np.nan)
-            bot_y.append(np.nan)
-    return np.array(top_y), np.array(bot_y)
+
+def process_image(path, fname, save_path, fmat, save=True, handdrawn=False):
+
+    # Open image
+    og_img = cv2.imread(os.path.join(path, fname))
+    img = cv2.cvtColor(np.copy(og_img), cv2.COLOR_BGR2GRAY)
     
-def get_points(img, left=False, right=False):
-    edges = cv2.Canny(np.copy(img), 0, 1)
-    w = img.shape[1]
-    x = [50, 75, 100, 125, 150, 175, 200]
-    for temp in list(reversed(x)):
-        x.append(w - temp)
-    if left:
-        x = x[:len(x) / 2]
-    elif right:
-        x = x[len(x) / 2:]
-    top_y, bot_y = _get_points(edges, x)
-    return np.array(x), np.array(top_y), np.array(bot_y)
-
-#def draw_line(img, m, b):
-#    x1 = 0
-#    x2 = img.shape[1]
-#    y1 = int(f(x1, m, b))
-#    y2 = int(f(x2, m, b))
-#    cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 20)
-#    return
-
-def _find_line(img, left=False, right=False):
-    x, top_y, bot_y = get_points(img, left=left, right=right)
-    ind = np.array(np.isfinite(top_y), ndmin=1)
-#    print(top_y[ind])
-#    print(bot_y[ind])
-    (m1, b1), r1, _, _, _ = np.polyfit(x[ind], top_y[ind], 1, full=True)
-    (m2, b2), r2, _, _, _ = np.polyfit(x[ind], bot_y[ind], 1, full=True)
-#    print(r1, r2)
-    return [[m1, b1], [m2, b2]], r1 + r2
-
-def find_line(img):
-    img = np.copy(img)
-    lines, r = _find_line(img)
-    if r < 200:
-        return lines
-
-    else:
-        lines, r = _find_line(img, right=True)
-        if r < 200:
-            return lines
-
-        else:
-            lines, r = _find_line(img, right=True)
-            if r < 200:
-                return lines
-
-    return None
-
-#def process_image(path, fname, save_path, fmat, save=True, handdrawn=False, use_edges=False):
-
-path = '../InputImages/'
-fmat = '.tif'
-fname = 'A10_2_S_ch2_t70h' + fmat
-handdrawn = False
-save = False
-use_edges = True
-
-# Open image
-og_img = cv2.imread(os.path.join(path, fname))
-img = cv2.cvtColor(np.copy(og_img), cv2.COLOR_BGR2GRAY)
-
-# Find lines for bounding box
-if handdrawn:  # Grabs lines drawn in handdrawn folder and applies to image
-    hd_path = os.path.join(path, 'handdrawn', fname)
-    if os.path.exists(hd_path):
-        hd_img = cv2.imread(hd_path)
-        hd_img = cv2.cvtColor(np.copy(hd_img), cv2.COLOR_BGR2GRAY)
-        hd_img[hd_img < 255] = 0
-        hd_img[hd_img > 0] = 50
-        lines = mod.find_lines_of_box(hd_img, hd=True)
-    else:
-        lines = None  # No handdrawn image found. Ignore this image.
-
-else:  # Handdrawn not being used. Find box with IA tools
-#    img = mod.exaggerate_bounding_box(img)
-
-    # Threshold
-    val = mod.filters.threshold_otsu(img)
-    img[img < val] = 0
-    img[img > 0] = 50
-    kernel = np.ones((3, 3), np.uint8)
-    img = cv2.erode(img, kernel, iterations=2)
-    lines = find_line(img)
-
-    if lines is None:  # First try failed
-        print('Fname:', fname)
-    #        lines = mod.find_lines_of_box(img, not use_edges)
-    
-    if lines is not None:
-        
-        # Remove empty space outside bounding box
-        img = mod.get_channel(og_img, lines)
-    
-        # Split wet and dry regions
-#        val = 49  #mod.thresh_img(img)
-        res = '\t'.join([str(x) for x in mod.stats(img, val)])  # Calc here before split
-    
-        # Show images
-        if not save:  # Just show
-            mod.plot_all_results(None, None, og_img, lines, img, val)
-        else:
-            mod.plot_all_results(save_path, fname.replace(fmat, ''), og_img, lines, img, val)
+    # Find lines for bounding box
+    if handdrawn:
+        # Grabs lines drawn in handdrawn folder and applies to image
+        hd_path = os.path.join(path, 'handdrawn', fname)
+        if os.path.exists(hd_path):
+            hd_img = cv2.imread(hd_path)
+            hd_img = cv2.cvtColor(np.copy(hd_img), cv2.COLOR_BGR2GRAY)
             
-#                # Report stats
-#                return res
-#        return ''
+            val = 255
+            hd_img = mod.threshold_img(hd_img, val)
+            lines = mod.find_lines_of_box(hd_img, hd=True)
+        else:
+            # No handdrawn image found. Ignore this image.
+            lines = None
+    
+    else:  # Handdrawn not being used. Find box with IA tools
+
+        # Threshold
+        val = mod.find_thresh(img)
+        img = mod.threshold_img(img, val)
+        
+        # Remove noise
+        kernel = np.ones((3, 3), np.uint8)
+        img = cv2.erode(img, kernel, iterations=2)
+        
+        # Find lines
+        lines = mod.find_lines(img)
+    
+    # Exit if lines not found
+    if lines is None:
+        print('Fname:', fname)
+        return ''
+        
+    # Remove empty space outside bounding box
+    img = mod.get_channel(og_img, lines)
+
+    # Split wet and dry regions
+    res = fname + '\t'
+    res = res + '\t'.join([str(x) for x in mod.stats(img, val)])
+
+    # Show images and save
+    mod.plot_all_results(save_path, fname.replace(fmat, ''), og_img,
+                         lines, img, val)
+        
+    # Report stats
+    return res
 
 
-#if __name__ == '__main__':
-#    
-#    # Parse input
-#    parser = argparse.ArgumentParser(description='Property calculation using cxcalc')
-#    parser.add_argument('path', type=str, help='path to folder root')
-#    parser.add_argument('-f','--format', help='Format of images',
-#                        required=False, default='.tif')
-#    parser.add_argument('-d', '--delim', required=False,
-#                        help='Delimiter for results file', default='\t')
-#    parser.add_argument('-s', action='store_false', default=True,
-#                    dest='save', help='Don\'t save images')
-#    parser.add_argument('-a', action='store_true', default=False,
-#                    dest='handdrawn', help='Use handdrawn lines')
-#    parser.add_argument('-u', action='store_true', default=False,
-#                    dest='use_edges', help='Use edges of channel only')
-#    args = parser.parse_args()
-#
-#    # Run
-#    main(args.path, args.format, args.delim, save=args.save,
-#         handdrawn=args.handdrawn, use_edges=args.use_edges)
+if __name__ == '__main__':
+    
+    # Parse input
+    parser = argparse.ArgumentParser(description='Property calculation using cxcalc')
+    parser.add_argument('path', type=str, help='path to folder root')
+    parser.add_argument('-f','--format', help='Format of images',
+                        required=False, default='.tif')
+    parser.add_argument('-d', '--delim', required=False,
+                        help='Delimiter for results file', default='\t')
+    parser.add_argument('-s', action='store_false', default=True,
+                    dest='save', help='Don\'t save images')
+    parser.add_argument('-a', action='store_true', default=False,
+                    dest='handdrawn', help='Use handdrawn lines')
+    args = parser.parse_args()
+
+    # Run
+    main(args.path, args.format, args.delim, save=args.save,
+         handdrawn=args.handdrawn)
--- module.py
+++ module.py
@@ -19,97 +19,94 @@ rgb_cividis = np.loadtxt('cividis.txt').T
 cmap = colors.ListedColormap(rgb_cividis.T, name='cividis')
 cm.register_cmap(name='cividis', cmap=cmap)
 
-def show(img, name=None, vmax=None, vmin=None, cmap='cividis', axis='off', origin='upper', norm=None):
-    plt.imshow(img, cmap=cmap, vmax=vmax, vmin=vmin, interpolation='none', origin=origin, norm=norm)
-    plt.axis(axis)
-    if name is not None:
-        plt.savefig(name, dpi=500, bbox_inches='tight')
-        plt.pause(0.005)
-        plt.ion()
-        plt.show()
-        plt.close()
 
-def show_wet_dry(img, val, name=None):    
-    temp_img = np.copy(img)
-    ind = np.where(np.logical_and(img >= 0, img < val))
-    temp_img[ind] = 1
-    ind = np.where(img >= val)
-    temp_img[ind] = 2
-    show(temp_img, name=name)
+#%% Image analysis and processing functions
 
-# Line: (rho, theta)
-# https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html
-def get_points(line, w):
-    rho, theta = line
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + w * (-b))
-    y1 = int(y0 + w * (a))
-    x2 = int(x0 - w * (-b))
-    y2 = int(y0 - w * (a))
-    return x1, y1, x2, y2
+def find_thresh(img):
+    return filters.threshold_otsu(img)
 
 
-def exaggerate_bounding_box(img, iterations=2):
-    img[img < filters.threshold_otsu(img)] = 0
-    img[img > 0] = 50
-    kernel = np.ones((5, 5), np.uint8)
-    img = cv2.erode(img, kernel, iterations=2)
-#    kernel = np.ones((200, 200), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=iterations)
-#    img = cv2.erode(img, kernel, iterations=iterations)
-#    kernel = np.ones((10, 10), np.uint8)
-#    img = cv2.erode(img, kernel)
+def threshold_img(img, val):
+    img[img < val] = 0
+    img[img > 0] = 1
     return img
 
-def find_lines_of_box(img, use_edges=False, hd=False):
-    edges = cv2.Canny(img, 0, 1)
+
+def _get_points(edges, x):
+    top_y = []
+    bot_y = []
+    for col in x:
+        points = np.where(edges[:, col] > 0)
+        if len(points[0]) > 0:
+            top_y.append(points[0][0])
+            bot_y.append(points[0][-1])
+        else:
+            top_y.append(np.nan)
+            bot_y.append(np.nan)
+    return np.array(top_y), np.array(bot_y)
+
+
+def get_points(img, left=False, right=False):
+    edges = cv2.Canny(np.copy(img), 0, 1)
     w = img.shape[1]
-    num_points = w / 4
-    if use_edges:
-        edges[:, 300:-300] = 0
-        num_points = 300
-    if hd:
-        res = 10
+    x = [50, 75, 100, 125, 150, 175, 200]
+    for temp in list(reversed(x)):
+        x.append(w - temp)
+    if left:
+        x = x[:len(x) / 2]
+    elif right:
+        x = x[len(x) / 2:]
+    top_y, bot_y = _get_points(edges, x)
+    return np.array(x), np.array(top_y), np.array(bot_y)
+
+
+def _find_lines(img, left=False, right=False):
+    x, top_y, bot_y = get_points(img, left=left, right=right)
+    ind = np.array(np.isfinite(top_y), ndmin=1)
+    (m1, b1), r1, _, _, _ = np.polyfit(x[ind], top_y[ind], 1, full=True)
+    (m2, b2), r2, _, _, _ = np.polyfit(x[ind], bot_y[ind], 1, full=True)
+    return [[m1, b1], [m2, b2]], r1 + r2
+
+
+def find_lines(img):
+    img = np.copy(img)
+    lines, r = _find_lines(img)
+    if r < 200:
+        return lines
+
     else:
-        res = 7
-    lines = cv2.HoughLines(edges, res, np.pi / 180, num_points)
-    if lines is not None:
-        lines = [x for x in lines if x[0][1] > 1 and x[0][1] < 2]
-#        print(len(lines))
-        temp1 = [x for x in lines[1:] if abs(x[0][0] - lines[0][0][0]) > 450]
-        if len(temp1) == 0:
-            temp1 = [x for x in lines[1:] if abs(x[0][0] - lines[0][0][0]) > 400]
-#        print(len(temp1))
-        temp2 = [x for x in temp1 if abs(x[0][1] - lines[0][0][1]) < 0.01]
-#        print(len(temp2))
-        if len(temp2) == 0:
-            temp2 = [x for x in temp1 if abs(x[0][1] - lines[0][0][1]) < 0.02]
-#            print(len(temp2))
-        lines = [lines[0]]
-        lines.extend(temp2)
-    if lines is None or len(lines) < 2:
-        return None
-    return lines
+        lines, r = _find_lines(img, right=True)
+        if r < 200:
+            return lines
 
-def get_equation(line, w):
-    rho, theta = line
-    points = get_points(line, w)
-    x1, y1, x2, y2 = [float(x) for x in points]
-    m = (y2 - y1) / (x2 - x1)
-    b = y1 - x1 * m
-    return m, b
+        else:
+            lines, r = _find_lines(img, right=True)
+            if r < 200:
+                return lines
+
+    return None
 
 
-#def draw_lines(img, lines):
-#    img = np.copy(img)
-#    for line in lines:
-#        for rho,theta in line:
-#            x1, y1, x2, y2 = get_points([rho, theta], img.shape[1])
-#            cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 20)
-#    return img
+def remove_pixels(img, line, above=True):
+    m, b = line
+    x = np.arange(img.shape[1])
+    y = m * x + b
+    for i in range(len(x)):
+        if above:
+            img[:y[i], x[i]] = np.nan
+        else:
+            img[y[i]:, x[i]] = np.nan
+    return img
+
+
+# Extract the actual channel from the image
+def get_channel(og_img, lines):
+    img = cv2.cvtColor(np.copy(og_img), cv2.COLOR_BGR2GRAY)
+    img = np.array(img, dtype=np.float)
+    img = remove_pixels(img, lines[0])
+    img = remove_pixels(img, lines[1], above=False)
+    return img
+
 
 def draw_lines(img, lines):
     img = np.copy(img)
@@ -120,6 +117,8 @@ def draw_lines(img, lines):
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 255), 20)
     return img
 
+
+#%% Reporting functions
 
 def stats(img, val):
     img = img[~np.isnan(img)]
@@ -135,39 +134,29 @@ def print_stats(img, val):
     dried_area, wet_area, perc = stats(img, val)
     print('%.2f mm2, %.2f mm2, %i perc' % (dried_area, wet_area, perc))
 
-def remove_pixels(img, line, above=True):
-    m, b = line
-    x = np.arange(img.shape[1])
-    y = m * x + b
-    for i in range(len(x)):
-        if above:
-            img[:y[i], x[i]] = np.nan
-        else:
-            img[y[i]:, x[i]] = np.nan
-    return img
+
+#%% Plotting functions
+
+def show(img, name=None, vmax=None, vmin=None, cmap='cividis', axis='off', origin='upper', norm=None):
+    plt.imshow(img, cmap=cmap, vmax=vmax, vmin=vmin, interpolation='none', origin=origin, norm=norm)
+    plt.axis(axis)
+    if name is not None:
+        plt.savefig(name, dpi=500, bbox_inches='tight')
+        plt.pause(0.005)
+        plt.ion()
+        plt.show()
+        plt.close()
 
 
-def thresh_img(img):
-    img = np.copy(img)
-    
-    # Remove NaN values (if any)
-    n = img.shape[0] * img.shape[1]
-    pixels = np.copy(img).reshape(n, 1)
-    ind = np.array(1 - np.isnan(pixels), dtype=bool)
-    pixels = pixels[ind]
-    
-    # Thresh
-    return filters.threshold_otsu(pixels)
+def show_wet_dry(img, val, name=None):    
+    temp_img = np.copy(img)
+    img[np.isnan(img)] = -1  # Avoids runtime warnings
+    ind = np.where(np.logical_and(img >= 0, img < val))
+    temp_img[ind] = 1
+    ind = np.where(img >= val)
+    temp_img[ind] = 2
+    show(temp_img, name=name)
 
-
-# Extract the actual channel from the image
-def get_channel(og_img, lines):
-    img = cv2.cvtColor(np.copy(og_img), cv2.COLOR_BGR2GRAY)
-    img = np.array(img, dtype=np.float)
-    img = remove_pixels(img, lines[0])
-    img = remove_pixels(img, lines[1], above=False)
-    return img
-    
 
 def plot_all_results(save_path, fname, og_img, lines, img, val):
     if save_path is not None and fname is not None:
@@ -187,3 +176,4 @@ def plot_all_results(save_path, fname, og_img, lines, img, val):
     show_wet_dry(img, val, name=fname3)
     if fname3 is None:
         plt.show()
+        
